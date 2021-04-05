4c704578aafccc848cbe60778b7a0c6423148abd
Kader Bouyakoub <bkader@mail.com>
2021-04-05 15:36:30 +0100

    fixed AllStats stats order (armor)

--- Modules/AllStats.lua
+++ Modules/AllStats.lua
@@ -51,18 +51,20 @@ KPack:AddModule("AllStats", "moves the functionality of the stat dropdowns to a 
 
     local AllStats_PrintStats
     do
-        local function AllStats_CreateStatFrame(name, text)
-            local frame =
-                CreateFrame("Frame", "KPackAllStatsAllStatsFrame" .. name, KPackAllStats, "StatFrameTemplate")
-            frame:SetWidth(128)
-            if text ~= nil then
-                local t = frame:CreateFontString(nil, "OVERLAY", "GameFontHighlightSmall")
-                t:SetPoint("BOTTOM", frame, "TOP", 0, -1)
-                t:SetText(text)
-                frame.text = text
-            end
-            return frame
-        end
+		local function AllStats_CreateStatFrame(name, text)
+		    local fname = "KPackAllStatsAllStatsFrame" .. name
+		    local frame = _G[fname]
+		    if not frame then
+		        frame = CreateFrame("Frame", fname, KPackAllStats, "StatFrameTemplate")
+		        frame:SetWidth(128)
+		        if text ~= nil then
+		            local t = frame:CreateFontString(nil, "OVERLAY", "GameFontHighlightSmall")
+		            t:SetPoint("BOTTOM", frame, "TOP", 0, -1)
+		            t:SetText(text)
+		        end
+		    end
+		    return frame
+		end
 
         function AllStats_PrintStats()
             -- strength
@@ -90,98 +92,121 @@ KPack:AddModule("AllStats", "moves the functionality of the stat dropdowns to a 
             spi:SetPoint("TOPLEFT", int, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetStat(spi, 5)
 
+            -- melee damage
             local md = AllStats_CreateStatFrame("MeleeDamage", PLAYERSTAT_MELEE_COMBAT)
             md:SetPoint("TOPLEFT", spi, "BOTTOMLEFT", 0, -11)
             PaperDollFrame_SetDamage(md)
             md:SetScript("OnEnter", CharacterDamageFrame_OnEnter)
 
+            -- melee speed
             local ms = AllStats_CreateStatFrame("MeleeSpeed")
             ms:SetPoint("TOPLEFT", md, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetAttackSpeed(ms)
 
+            -- melee power
             local mp = AllStats_CreateStatFrame("MeleePower")
             mp:SetPoint("TOPLEFT", ms, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetAttackPower(mp)
 
+            -- melee hit
             local mh = AllStats_CreateStatFrame("MeleeHit")
             mh:SetPoint("TOPLEFT", mp, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetRating(mh, CR_HIT_MELEE)
 
+            -- melee crit
             local mc = AllStats_CreateStatFrame("MeleeCrit")
             mc:SetPoint("TOPLEFT", mh, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetMeleeCritChance(mc)
 
+            -- expertise
             local me = AllStats_CreateStatFrame("MeleeExpert")
             me:SetPoint("TOPLEFT", mc, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetExpertise(me)
 
+            -- ranged damage
             local rd = AllStats_CreateStatFrame("RangeDamage", PLAYERSTAT_RANGED_COMBAT)
             rd:SetPoint("TOPLEFT", me, "BOTTOMLEFT", 0, -11)
             PaperDollFrame_SetRangedDamage(rd)
             rd:SetScript("OnEnter", CharacterRangedDamageFrame_OnEnter)
 
+            -- ranged speed
             local rs = AllStats_CreateStatFrame("RangeSpeed")
             rs:SetPoint("TOPLEFT", rd, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetRangedAttackSpeed(rs)
 
+            -- ranged power
             local rp = AllStats_CreateStatFrame("RangePower")
             rp:SetPoint("TOPLEFT", rs, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetRangedAttackPower(rp)
 
+            -- ranged hit
             local rh = AllStats_CreateStatFrame("RangeHit")
             rh:SetPoint("TOPLEFT", rp, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetRating(rh, CR_HIT_RANGED)
 
+            -- ranged crit
             local rc = AllStats_CreateStatFrame("RangeCrit")
             rc:SetPoint("TOPLEFT", rh, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetRangedCritChance(rc)
 
+            -- spell damage
             local sd = AllStats_CreateStatFrame("SpellDamage", PLAYERSTAT_SPELL_COMBAT)
             sd:SetPoint("TOPLEFT", rc, "BOTTOMLEFT", 0, -11)
             PaperDollFrame_SetSpellBonusDamage(sd)
             sd:SetScript("OnEnter", CharacterSpellBonusDamage_OnEnter)
 
+            -- spell healing
             local she = AllStats_CreateStatFrame("SpellHeal")
             she:SetPoint("TOPLEFT", sd, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetSpellBonusHealing(she)
 
+            -- spell hit
             local shi = AllStats_CreateStatFrame("SpellHit")
             shi:SetPoint("TOPLEFT", she, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetRating(shi, CR_HIT_SPELL)
 
+            -- spell crit
             local sc = AllStats_CreateStatFrame("SpellCrit")
             sc:SetPoint("TOPLEFT", shi, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetSpellCritChance(sc)
             sc:SetScript("OnEnter", CharacterSpellCritChance_OnEnter)
 
+            -- spell haste
             local sha = AllStats_CreateStatFrame("SpellHaste")
             sha:SetPoint("TOPLEFT", sc, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetSpellHaste(sha)
 
+            -- spell regen
             local sr = AllStats_CreateStatFrame("SpellRegen")
             sr:SetPoint("TOPLEFT", sha, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetManaRegen(sr)
 
-            local armor = AllStats_CreateStatFrame("Armor")
-            armor:SetPoint("TOPLEFT", sr, "BOTTOMLEFT", 0, 1)
+            -- armor
+            local armor = AllStats_CreateStatFrame("Armor", PLAYERSTAT_DEFENSES)
+            armor:SetPoint("TOPLEFT", sr, "BOTTOMLEFT", 0, -11)
             PaperDollFrame_SetArmor(armor)
 
-            local def = AllStats_CreateStatFrame("Defense", PLAYERSTAT_DEFENSES)
-            def:SetPoint("TOPLEFT", armor, "BOTTOMLEFT", 0, -11)
+            -- defense
+            local def = AllStats_CreateStatFrame("Defense")
+            def:SetPoint("TOPLEFT", armor, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetDefense(def)
 
+            -- dodge
             local dodge = AllStats_CreateStatFrame("Dodge")
             dodge:SetPoint("TOPLEFT", def, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetDodge(dodge)
 
+            -- parry
             local parry = AllStats_CreateStatFrame("Parry")
             parry:SetPoint("TOPLEFT", dodge, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetParry(parry)
 
+            -- block
             local block = AllStats_CreateStatFrame("Block")
             block:SetPoint("TOPLEFT", parry, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetBlock(block)
 
+            -- resilience
             local res = AllStats_CreateStatFrame("Resil")
             res:SetPoint("TOPLEFT", block, "BOTTOMLEFT", 0, 1)
             PaperDollFrame_SetResilience(res)
