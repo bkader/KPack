a7e011d3c764b30b1f86c11480c40e4d123615a3
Kader Bouyakoub <bkader@mail.com>
2021-04-30 14:24:53 +0100

    added auto invites and fixed sub-modules not being disabled

--- KPack/Modules/RaidUtility.lua
+++ KPack/Modules/RaidUtility.lua
@@ -14,7 +14,7 @@ KPack:AddModule("RaidUtility", function(_, core, L)
 	core.LSM = LSM
 
 	local pairs, ipairs, select = pairs, ipairs, select
-	local tinsert, tsort = table.insert, table.sort
+	local tinsert, tremove, tsort = table.insert, table.remove, table.sort
 	local strformat, strfind, strlower, strlen = string.format, string.find, string.lower, string.len
 	local CreateFrame = CreateFrame
 	local GetNumRaidMembers = GetNumRaidMembers
@@ -51,6 +51,19 @@ KPack:AddModule("RaidUtility", function(_, core, L)
 			f:Show()
 		elseif not cond and f:IsShown() then
 			f:Hide()
+		end
+	end
+
+	function mod:InGroup()
+		return (GetNumRaidMembers() > 0 or GetNumPartyMembers() > 0)
+	end
+
+	function mod:IsPromoted(name)
+		name = name or "player"
+		if UnitInRaid(name) then
+			return UnitIsRaidOfficer(name), "raid"
+		elseif UnitInParty(name) then
+			return UnitIsPartyLeader(name), "party"
 		end
 	end
 
@@ -504,15 +517,6 @@ KPack:AddModule("RaidUtility", function(_, core, L)
 				self:Hide()
 			end
 		end)
-
-		function mod:IsPromoted(name)
-			name = name or "player"
-			if UnitInRaid(name) then
-				return UnitIsRaidOfficer(name), "raid"
-			elseif UnitInParty(name) then
-				return UnitIsPartyLeader(name), "party"
-			end
-		end
 
 		function HandleLootMethod()
 			if not DB.Loot.enabled then
@@ -1152,17 +1156,19 @@ KPack:AddModule("RaidUtility", function(_, core, L)
 
 		core:RegisterForEvent("PLAYER_ENTERING_WORLD", function()
 			SetupDatabase()
-			if DB.Auras.enabled then
-				ShowDisplay()
-			else
-				HideDisplay()
-			end
 
 			if DB.Auras.locked then
 				LockDisplay()
 			else
 				UnlockDisplay()
 			end
+
+			if DB.Auras.enabled then
+				ShowDisplay()
+			else
+				HideDisplay()
+			end
+
 			SLASH_KPACKAURAS1 = "/auras"
 			SlashCmdList.KPACKAURAS = function()
 				core:OpenConfig("RaidUtility", "Auras")
@@ -1643,17 +1649,18 @@ KPack:AddModule("RaidUtility", function(_, core, L)
 
 		core:RegisterForEvent("PLAYER_ENTERING_WORLD", function()
 			SetupDatabase()
-			if DB.Sunders.enabled then
-				sunders = DB.Sunders.sunders
-				ShowDisplay()
-			else
-				HideDisplay()
-			end
 
 			if DB.Sunders.locked then
 				LockDisplay()
 			else
 				UnlockDisplay()
+			end
+
+			if DB.Sunders.enabled then
+				sunders = DB.Sunders.sunders
+				ShowDisplay()
+			else
+				HideDisplay()
 			end
 
 			SLASH_KPACKSUNDER1 = "/sunder"
@@ -2299,17 +2306,18 @@ KPack:AddModule("RaidUtility", function(_, core, L)
 			end
 			core.After(5, CacheHealers)
 
+			if DB.Mana.locked then
+				LockDisplay()
+			else
+				UnlockDisplay()
+			end
+
 			if DB.Mana.enabled then
 				ShowDisplay()
 			else
 				HideDisplay()
 			end
 
-			if DB.Mana.locked then
-				LockDisplay()
-			else
-				UnlockDisplay()
-			end
 			SLASH_KPACKHEALERSMANA1 = "/mana"
 			SLASH_KPACKHEALERSMANA2 = "/healersmana"
 			SlashCmdList.KPACKHEALERSMANA = function()
@@ -2587,6 +2595,7 @@ KPack:AddModule("RaidUtility", function(_, core, L)
 					type = "group",
 					name = L["Raid Cooldowns"],
 					childGroups = "tab",
+					order = order,
 					get = function(i)
 						return DB.Cooldowns[i[#i]]
 					end,
@@ -3105,6 +3114,13 @@ KPack:AddModule("RaidUtility", function(_, core, L)
 		order = order + 1
 		core:RegisterForEvent("PLAYER_ENTERING_WORLD", function()
 			SetupDatabase()
+
+			if DB.Cooldowns.locked then
+				LockDisplay()
+			else
+				UnlockDisplay()
+			end
+
 			if DB.Cooldowns.enabled then
 				ShowDisplay()
 			else
@@ -3115,6 +3131,319 @@ KPack:AddModule("RaidUtility", function(_, core, L)
 			SlashCmdList.KPACKCOOLDOWNS = function()
 				core:OpenConfig("RaidUtility", "Cooldowns")
 			end
+		end)
+	end
+
+	---------------------------------------------------------------------------
+	-- Auto Invites
+
+	do
+		defaults.Invites = {keyword = "", guidkeyword = ""}
+
+		local inGuild
+		local guildRanks, GetGuildRanks = {}
+		local options, GetOptions
+		local keyword, guidkeyword
+
+		local DoActualInvites, DoGuildInvites
+		local inviteFrame, inviteQueue = CreateFrame("Frame"), {}
+
+		local function CanInvite()
+			return (mod:InGroup() and mod:IsPromoted()) or not mod:InGroup()
+		end
+
+		local function InviteGuild()
+			if CanInvite() then
+				GuildRoster()
+				SendChatMessage(L["All max level characters will be invited to raid in 10 seconds. Please leave your groups."], "GUILD")
+				core.After(10, function() DoGuildInvites(MAX_PLAYER_LEVEL) end)
+			end
+		end
+
+		local function InviteZone()
+			if CanInvite() then
+				GuildRoster()
+				local zone = GetRealZoneText()
+				SendChatMessage(L:F("All characters in %s will be invited to raid in 10 seconds. Please leave your groups.", zone), "GUILD")
+				core.After(10, function() DoGuildInvites(nil, zone) end)
+			end
+		end
+
+		local function InviteRank(rank, name)
+			if CanInvite() then
+				GuildRoster()
+				GuildControlSetRank(rank)
+
+				local ochat = select(3, GuildControlGetRankFlags())
+				local channel = ochat and "OFFICER" or "GUILD"
+				SendChatMessage(L:F("All characters of rank %s or higher will be invited to raid in 10 seconds. Please leave your groups.", name), "GUILD")
+				core.After(10, function() DoGuildInvites(nil, nil, rank) end)
+			end
+		end
+
+		local function _convertToRaid(self, elapsed)
+			self.elapsed = (self.elapsed or 0) + elapsed
+			if self.elapsed > 1 then
+				self.elapsed = 0
+				if UnitInRaid("player") then
+					DoActualInvites()
+					self:SetScript("OnUpdate", nil)
+				end
+			end
+		end
+
+		local function _waitForParty(self, elapsed)
+			self.elapsed = (self.elapsed or 0) + elapsed
+			if self.elapsed > 1 then
+				self.elapsed = 0
+				if GetNumPartyMembers() > 0 then
+					ConvertToRaid()
+					self:SetScript("OnUpdate", _convertToRaid)
+				end
+			end
+		end
+
+		function DoActualInvites()
+			if not UnitInRaid("player") then
+				local num = GetNumPartyMembers() + 1
+				if num == 5 then
+					if #inviteQueue > 0 then
+						ConvertToRaid()
+						inviteFrame:SetScript("OnUpdate", _convertToRaid)
+					end
+				else
+					local tmp = {}
+					for i = 1, (5 - num) do
+						local u = tremove(inviteQueue)
+						if u then
+							tmp[u] = true
+						end
+					end
+					if #inviteQueue > 0 then
+						inviteFrame:SetScript("OnUpdate", _waitForParty)
+					end
+					for k in pairs(tmp) do
+						InviteUnit(k)
+					end
+				end
+				return
+			end
+			for _, v in next, inviteQueue do
+				InviteUnit(v)
+			end
+			inviteQueue = {}
+		end
+
+		function DoGuildInvites(level, zone, rank)
+			for i = 1, GetNumGuildMembers() do
+				local name, _, rankindex, unitlevel, _, unitzone, _, _, online = GetGuildRosterInfo(i)
+				if name and online and not UnitInParty(name) and not UnitInRaid(name) then
+					if
+						(level and level <= unitlevel) or (zone and zone == unitzone) or
+							(rank and (rankindex + 1) <= rank)
+					 then
+						inviteQueue[#inviteQueue + 1] = name
+					end
+				end
+			end
+			DoActualInvites()
+		end
+
+		function ListGuildRanks()
+			if inGuild and not next(guildRanks) then
+				for i = 1, GuildControlGetNumRanks() do
+					local rankname = GuildControlGetRankName(i)
+					tinsert(guildRanks, i, rankname)
+				end
+				return guildRanks
+			end
+			return guildRanks
+		end
+
+		function GetOptions()
+			if not options then
+				inGuild = inGuild or IsInGuild()
+
+				options = {
+					type = "group",
+					name = L["Auto Invites"],
+					order = order,
+					args = {
+						quickinvite = {
+							type = "group",
+							inline = true,
+							name = L["Quick Invites"],
+							order = 1,
+							disabled = function()
+								return not inGuild
+							end,
+							hidden = function()
+								return not inGuild
+							end,
+							args = {
+								guild = {
+									type = "execute",
+									name = L["Invite guild"],
+									desc = L["Invite everyone in your guild at the maximum level."],
+									order = 2,
+									disabled = function()
+										return not CanInvite()
+									end,
+									func = InviteGuild
+								},
+								zone = {
+									type = "execute",
+									name = L["Invite zone"],
+									desc = L["Invite everyone in your guild who are in the same zone as you."],
+									order = 3,
+									disabled = function()
+										return not CanInvite()
+									end,
+									func = InviteZone
+								}
+							}
+						},
+						keywordinvite = {
+							type = "group",
+							inline = true,
+							name = L["Keyword invite"],
+							order = 2,
+							args = {
+								keyword = {
+									type = "input",
+									name = L["Keyword"],
+									desc = L["Anyone who whispers you this keyword will automatically and immediately be invited to your group."],
+									order = 1,
+									get = function()
+										return DB.Invites.keyword
+									end,
+									set = function(_, val)
+										DB.Invites.keyword = val:trim()
+										keyword = (DB.Invites.keyword ~= "") and DB.Invites.keyword or nil
+									end
+								},
+								guidkeyword = {
+									type = "input",
+									name = L["Guild Keyword"],
+									desc = L["Any guild member who whispers you this keyword will automatically and immediately be invited to your group."],
+									order = 2,
+									disabled = function()
+										return not inGuild
+									end,
+									get = function()
+										return DB.Invites.guidkeyword
+									end,
+									set = function(_, val)
+										DB.Invites.guidkeyword = val:trim()
+										guidkeyword =
+											(DB.Invites.guidkeyword ~= "") and DB.Invites.guidkeyword or nil
+									end
+								}
+							}
+						},
+						rankinvite = {
+							type = "group",
+							inline = true,
+							name = L["Rank Invites"],
+							descStyle = "inline",
+							order = 3,
+							disabled = function()
+								return not inGuild
+							end,
+							hidden = function()
+								return not inGuild
+							end,
+							args = {
+								desc = {
+									type = "description",
+									name = L["Clicking any of the buttons below will invite anyone of the selected rank AND HIGHER to your group. So clicking the 3rd button will invite anyone of rank 1, 2 or 3, for example. It will first post a message in either guild or officer chat and give your guild members 10 seconds to leave their groups before doing the actual invites."],
+									width = "full",
+									order = 0
+								}
+							}
+						}
+					}
+				}
+			end
+			return options
+		end
+
+		local function IsGuildMember(name)
+			inGuild = inGuild or IsInGuild()
+			if inGuild then
+				for i = 1, GetNumGuildMembers() do
+					local n = GetGuildRosterInfo(i)
+					if n == name then
+						return true
+					end
+				end
+			end
+			return false
+		end
+
+		inviteFrame:SetScript("OnEvent", function(self, event, msg, sender)
+			if (keyword and msg == keyword) or (guidkeyword and msg == guidkeyword and IsGuildMember(sender) and CanInvite()) then
+				local inInstance, instanceType = IsInInstance()
+				local numparty, numraid = GetNumPartyMembers(), GetNumRaidMembers()
+				if inInstance and instanceType == "party" and party == 4 then
+					SendChatMessage(L["Sorry, the group is full."], "WHISPER", nil, sender)
+				elseif party == 4 and raid == 0 then
+					inviteQueue[#inviteQueue + 1] = sender
+					DoActualInvites()
+				elseif raid == 40 then
+					SendChatMessage(L["Sorry, the group is full."], "WHISPER", nil, sender)
+				else
+					InviteUnit(sender)
+				end
+			end
+		end)
+
+		core:RegisterForEvent("PLAYER_LOGIN", function()
+			SetupDatabase()
+
+			GuildRoster()
+			inGuild = IsInGuild()
+
+			options = options or GetOptions()
+			if inGuild then
+				local ranks, numorder = ListGuildRanks(), 1
+				for i, name in ipairs(ranks) do
+					options.args.rankinvite.args[name .. i] = {
+						type = "execute",
+						name = name,
+						desc = L:F("Invite all guild members of rank %s or higher.", name),
+						order = numorder,
+						func = function()
+							InviteRank(i, name)
+						end,
+						disabled = function()
+							return not CanInvite()
+						end
+					}
+					numorder = numorder + 1
+				end
+			end
+			mod.options.args.Invites = options
+			order = order + 1
+
+			if DB.Invites.keyword and DB.Invites.keyword:trim() ~= "" then
+				keyword = DB.Invites.keyword
+			end
+			if DB.Invites.guidkeyword and DB.Invites.guidkeyword:trim() ~= "" then
+				guidkeyword = DB.Invites.guidkeyword
+			end
+
+			if keyword or guidkeyword then
+				inviteFrame:RegisterEvent("CHAT_MSG_BN_WHISPER")
+				inviteFrame:RegisterEvent("CHAT_MSG_WHISPER")
+			else
+				inviteFrame:UnregisterAllEvents()
+			end
+		end)
+		core:RegisterForEvent("GUILD_ROSTER_UPDATE", function()
+			inGuild = IsInGuild()
+			wipe(guildRanks)
+			guildRanks = inGuild and ListGuildRanks() or {}
 		end)
 	end
 
