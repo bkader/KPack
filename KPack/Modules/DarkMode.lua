d01da707e658ef2d0fb8ebbc2f6178bffa900151
Kader Bouyakoub <bkader@mail.com>
2022-02-13 10:07:25 +0100

    DarkMode: added color option

--- KPack/Modules/DarkMode.lua
+++ KPack/Modules/DarkMode.lua
@@ -1,6 +1,12 @@
 assert(KPack, "KPack not found!")
-KPack:AddModule("Dark Mode", function(_, core)
+KPack:AddModule("Dark Mode", function(_, core, L)
 	if core:IsDisabled("Dark Mode") then return end
+
+	local DB
+	local defaults = {
+		normal = {r = 0.37, g = 0.37, b = 0.37, a = 1},
+		equipped = {r = 0.1, g = 0.5, b = 0.1, a = 1}
+	}
 
 	local config = {
 		textures = {
@@ -9,11 +15,11 @@ KPack:AddModule("Dark Mode", function(_, core)
 			hightlight = [[Interface\Addons\KPack\Media\Textures\AB_Hightlight]],
 			pushed = [[Interface\Addons\KPack\Media\Textures\AB_Pushed]],
 			checked = [[Interface\Addons\KPack\Media\Textures\AB_Checked]],
-			equipped = [[Interface\Addons\KPack\Media\Textures\AB_Equipped]],
+			equipped = [[Interface\Addons\KPack\Media\Textures\AB_Equipped]]
 		},
 		colors = {
-			normal = {r = 0.37, g = 0.37, b = 0.37},
-			equipped = {r = 0.1, g = 0.5, b = 0.1}
+			normal = {r = 0.37, g = 0.37, b = 0.37, a = 1},
+			equipped = {r = 0.1, g = 0.5, b = 0.1, a = 1}
 		}
 	}
 
@@ -164,7 +170,7 @@ KPack:AddModule("Dark Mode", function(_, core)
 			t:SetAllPoints(btn)
 			t:SetTexture(config.textures.equipped)
 			if btn.action and IsEquippedAction(btn.action) then
-				t:SetVertexColor(0, 1, 0, 0.65)
+				t:SetVertexColor(config.colors.equipped.r or 0, config.colors.equipped.g or 1, config.colors.equipped.b or 0, config.colors.equipped.a or 0.65)
 				t:Show()
 			elseif btn.action then
 				t:Hide()
@@ -172,7 +178,7 @@ KPack:AddModule("Dark Mode", function(_, core)
 			local _SetVertexColor = t.SetVertexColor
 			t.SetVertexColor = function(self, r, g, b, a)
 				if btn.action and IsEquippedAction(btn.action) then
-					_SetVertexColor(self, 0, 1, 0, 0.65)
+					_SetVertexColor(self, config.colors.equipped.r or 0, config.colors.equipped.g or 1, config.colors.equipped.b or 0, config.colors.equipped.a or 0.65)
 				else
 					_SetVertexColor(self, r, g, b, a)
 				end
@@ -195,41 +201,33 @@ KPack:AddModule("Dark Mode", function(_, core)
 		t = _G[name .. "NormalTexture2"] or _G[name .. "NormalTexture"] or btn.GetNormalTexture and btn:GetNormalTexture()
 		if t then
 			if btn.action and IsEquippedAction(btn.action) then
-				t:SetVertexColor(config.colors.equipped.r, config.colors.equipped.g, config.colors.equipped.b, 1)
+				t:SetVertexColor(config.colors.equipped.r, config.colors.equipped.g, config.colors.equipped.b, config.colors.equipped.a or 1)
 			else
-				t:SetVertexColor(config.colors.normal.r, config.colors.normal.g, config.colors.normal.b, 1)
+				t:SetVertexColor(config.colors.normal.r, config.colors.normal.g, config.colors.normal.b, config.colors.normal.a or 1)
 			end
 			t:SetAllPoints(btn)
 			hooksecurefunc(t, "SetVertexColor", function(self, r, g, b, a)
-					local bn = self:GetParent()
-					if r == 1 and g == 1 and b == 1 and bn.action and (IsEquippedAction(btn.action)) then
-					if
-						config.colors.equipped.r == 1 and config.colors.equipped.g == 1 and
-							config.colors.equipped.b == 1
-					 then
+				local bn = self:GetParent()
+				if r == 1 and g == 1 and b == 1 and bn.action and (IsEquippedAction(btn.action)) then
+					if config.colors.equipped.r == 1 and config.colors.equipped.g == 1 and config.colors.equipped.b == 1 then
 						self:SetVertexColor(0.99, 0.99, 0.99, 1)
 					else
-						self:SetVertexColor(config.colors.equipped.r, config.colors.equipped.g, config.colors.equipped.b, 1)
+						self:SetVertexColor(config.colors.equipped.r, config.colors.equipped.g, config.colors.equipped.b, config.colors.equipped.a or 1)
 					end
 				elseif r == 0.5 and g == 0.5 and b == 1 then
-					if
-						config.colors.normal.r == 0.5 and
-						config.colors.normal.g == 0.5 and
-						config.colors.normal.b == 1
-					then
+					if config.colors.normal.r == 0.5 and config.colors.normal.g == 0.5 and config.colors.normal.b == 1 then
 						self:SetVertexColor(0.49, 0.49, 0.99, 1)
 					else
-						self:SetVertexColor(config.colors.normal.r, config.colors.normal.g, config.colors.normal.b, 1)
+						self:SetVertexColor(config.colors.normal.r, config.colors.normal.g, config.colors.normal.b, config.colors.normal.a or 1)
 					end
 				elseif r == 1 and g == 1 and b == 1 then
 					if
-						config.colors.normal.r == 1 and
-						config.colors.normal.g == 1 and
-						config.colors.normal.b == 1
-					then
+						config.colors.normal.r == 1 and config.colors.normal.g == 1 and
+							config.colors.normal.b == 1
+					 then
 						self:SetVertexColor(0.99, 0.99, 0.99, 1)
 					else
-						self:SetVertexColor(config.colors.normal.r, config.colors.normal.g, config.colors.normal.b, 1)
+						self:SetVertexColor(config.colors.normal.r, config.colors.normal.g, config.colors.normal.b, config.colors.normal.a or 1)
 					end
 				end
 			end)
@@ -341,7 +339,7 @@ KPack:AddModule("Dark Mode", function(_, core)
 	local function DarkMode()
 		for _, f in pairs(framesList) do
 			if _G[f] then
-				_G[f]:SetVertexColor(config.colors.normal.r, config.colors.normal.g, config.colors.normal.b, 1)
+				_G[f]:SetVertexColor(config.colors.normal.r, config.colors.normal.g, config.colors.normal.b, config.colors.normal.a or 1)
 			end
 		end
 
@@ -362,5 +360,152 @@ KPack:AddModule("Dark Mode", function(_, core)
 		Darken_BagButton("MainMenuBarBackpackButton")
 	end
 
+	local function SetupDatabase()
+		if not DB then
+			if type(core.db.DarkMode) ~= "table" or not next(core.db.DarkMode) then
+				core.db.DarkMode = CopyTable(defaults)
+			end
+
+			DB = core.db.DarkMode
+		end
+
+		if core.db.DarkMode.charspecific then
+			if type(core.char.DarkMode) ~= "table" or not next(core.char.DarkMode) then
+				core.char.DarkMode = CopyTable(core.db.DarkMode)
+			end
+			DB = core.char.DarkMode
+			DB.charspecific = nil
+		end
+
+		if DB.classcolor and core.mycolor then
+			config.colors.normal.r = core.mycolor.r
+			config.colors.normal.g = core.mycolor.g
+			config.colors.normal.b = core.mycolor.b
+			config.colors.normal.a = 1
+
+			-- equipped;
+			if DB.equipped then
+				config.colors.equipped.r = DB.equipped.r or defaults.equipped.r
+				config.colors.equipped.g = DB.equipped.g or defaults.equipped.g
+				config.colors.equipped.b = DB.equipped.b or defaults.equipped.b
+				config.colors.equipped.a = DB.equipped.a or defaults.equipped.a
+			end
+		else
+			for k, _ in pairs(defaults) do
+				if DB[k] then
+					config.colors[k].r = DB[k].r or defaults[k].r
+					config.colors[k].g = DB[k].g or defaults[k].g
+					config.colors[k].b = DB[k].b or defaults[k].b
+					config.colors[k].a = DB[k].a or defaults[k].a
+				end
+			end
+		end
+	end
+
+	local function SetupOptions()
+		SetupDatabase()
+
+		if not core.options.args.Options.args.DarkMode then
+			core.options.args.Options.args.DarkMode = {
+				type = "group",
+				name = L["Dark Mode"],
+				get = function(i)
+					local c = config.colors[i[#i]]
+					return c.r or 1, c.g or 1, c.b or 1, c.a or 1
+				end,
+				set = function(i, r, g, b, a)
+					DB[i[#i]].r = r or 1
+					DB[i[#i]].g = g or 1
+					DB[i[#i]].b = b or 1
+					DB[i[#i]].a = a or 1
+					SetupDatabase()
+					DarkMode()
+				end,
+				args = {
+					charspecific = {
+						type = "toggle",
+						name = L["Character Specific"],
+						desc = L["Enable this if you want settings to be stored per character rather than per account."],
+						get = function()
+							return core.db.DarkMode.charspecific
+						end,
+						set = function()
+							core.db.DarkMode.charspecific = not core.db.DarkMode.charspecific
+							if not core.db.DarkMode.charspecific then
+								core.db.DarkMode.charspecific = nil
+								core.char.DarkMode = nil
+								DB = nil
+							end
+							SetupDatabase()
+							DarkMode()
+						end,
+						order = 1
+					},
+					classcolor = {
+						type = "toggle",
+						name = L["Use class color"],
+						get = function()
+							return DB.classcolor
+						end,
+						set = function()
+							DB.classcolor = not DB.classcolor
+							SetupDatabase()
+							DarkMode()
+						end,
+						order = 2
+					},
+					sep_1 = {
+						type = "description",
+						name = " ",
+						order = 3,
+						width = "full"
+					},
+					normal = {
+						type = "color",
+						name = L["Color"],
+						hasAlpha = true,
+						order = 4,
+						disabled = function()
+							return DB.classcolor
+						end
+					},
+					equipped = {
+						type = "color",
+						name = CURRENTLY_EQUIPPED,
+						hasAlpha = true,
+						order = 5
+					},
+					sep_2 = {
+						type = "description",
+						name = " ",
+						order = 6,
+						width = "full"
+					},
+					reset = {
+						type = "execute",
+						name = RESET,
+						order = 99,
+						width = "double",
+						confirm = function()
+							return L:F("Are you sure you want to reset %s to default?", L["Dark Mode"])
+						end,
+						func = function()
+							DB.classcolor = nil
+							for k, v in pairs(defaults) do
+								DB[k].r, DB[k].g, DB[k].b, DB[k].a = v.r, v.g, v.b, v.a
+								config.colors.r, config.colors.g, config.colors.b, config.colors.a = v.r, v.g, v.b, v.a
+							end
+
+							SetupDatabase()
+							DarkMode()
+							collectgarbage("collect")
+						end
+					}
+				}
+			}
+		end
+	end
+
+	core:RegisterForEvent("PLAYER_LOGIN", SetupOptions)
 	core:RegisterForEvent("PLAYER_ENTERING_WORLD", DarkMode)
 end)
